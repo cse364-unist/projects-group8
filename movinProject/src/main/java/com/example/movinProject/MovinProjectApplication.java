cd6067ce2b12271a69b806e4a3046c5354ca0777
hyunsu Ye <dpgustn1@unist.ac.kr>
2024-05-02 23:37:05 +0900

    delete data preload

--- movinProject/src/main/java/com/example/movinProject/MovinProjectApplication.java
+++ movinProject/src/main/java/com/example/movinProject/MovinProjectApplication.java
@@ -2,21 +2,10 @@ package com.example.movinProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.movinProject.CsvLoader;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class MovinProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovinProjectApplication.class, args);
 	}
-	@Bean
-    public CommandLineRunner loadMovieData(CsvLoader csvLoader) {
-        return args -> {
-            System.out.println("Working Directory = " + System.getProperty("user.dir"));
-            csvLoader.loadCsvDataToDB("./movinProject/data/movies.csv");
-            csvLoader.loadCsvDataToDB("./movinProject/data/ratings.csv");
-        };
-    }
 }
