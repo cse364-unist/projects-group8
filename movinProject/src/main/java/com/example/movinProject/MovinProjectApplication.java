d741a9ae43f88317a4eb989f26b6ed2aca16045b
hyunsu Ye <dpgustn1@unist.ac.kr>
2024-06-06 03:30:12 +0900

    tomcat server setting

--- movinProject/src/main/java/com/example/movinProject/MovinProjectApplication.java
+++ movinProject/src/main/java/com/example/movinProject/MovinProjectApplication.java
@@ -2,9 +2,10 @@ package com.example.movinProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MovinProjectApplication {
+public class MovinProjectApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MovinProjectApplication.class, args);
 	}
