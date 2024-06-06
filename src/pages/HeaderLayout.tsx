1630f871380769f3ebbb1709a2a541d87a1caa75
GongBJ <gongbj0113@gmail.com>
2024-06-06 17:20:02 +0900

    header layout skeleton

--- src/pages/HeaderLayout.tsx
+++ src/pages/HeaderLayout.tsx
@@ -1,3 +1,11 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+
 export default function HeaderLayout() {
-  return <div></div>;
+  return (
+    <main>
+      <Header />
+      <Outlet />
+    </main>
+  );
 }
