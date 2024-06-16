addaee8f53c5ac49f1b20ca1396b082995a3d930
GongBJ <gongbj0113@gmail.com>
2024-06-16 18:37:33 +0900

    api base path

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,12 @@
-export const baseApiUrl = 'http://localhost:8080';
-export const baseWsUrl = 'ws://localhost:8080';
+export const baseApiUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'http://localhost:8080/api';
+
+export const baseWsUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'ws://localhost:8080'
+    : 'ws://localhost:8080/api';
 
 export const desktopContentAreaWidth = 1200;
 export const desktopContentAreaPadding = 20;
